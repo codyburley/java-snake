9d6383a64c74d120720269c669b7f38dfecc7a44
Cody Burley <codyburley@gmail.com>
2023-04-25 14:11:55 -0400

    finished base game

--- src/GameFrame.java
+++ src/GameFrame.java
@@ -1,7 +1,7 @@
 import javax.swing.JFrame;
 
 public class GameFrame extends JFrame {
-  GameFrame(){
+  GameFrame() {
     this.add(new GamePanel());
     this.setTitle("Snake");
     this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
